a21a27e862b19e420829096d0d2d64282a0f157a
Henrik <thkodehode@gmail.com>
2025-02-24 10:08:46 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Hello, world!');
+
+// var x = 5 we do use use var anymore
+
+const z = 50
+
+let firstName = 'Emil' // string
+let age = 30 // Integer
+let exampleFloat = 30.2 // Float
+let fromBergen = true // Boolean
+let exampleArray = ['Mazda', ' Open', 'Saab'] // Array with text strings
+let myObject = {
+    firstName: 'Torleif', 
+    lastName: 'Leiknes',
+    age: 67, 
+    eMail: 'torlief@leiknes.no'
+}
+
+console.log(myObject)
+
+let x = 11
+let y = 4
+
+x++
+console.log(x)
